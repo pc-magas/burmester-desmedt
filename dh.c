6736dc4fd7841f6902a512266197ae3dddbe5b47
Dimitrios Desyllas (pc_magas) <you@example.com>
2019-01-25 18:13:35 +0200

    Add Comments

--- dh.c
+++ dh.c
@@ -1,6 +1,11 @@
 #include "dh.h"
 #include <openssl/dh.h>
 
+/**
+ * Generate Diffie-Hellman Key Agreement keys.
+ * @param [out] encryptionInfo The created Kerys Structure
+ * @return 0 on sucess -1 on failure
+ */
 int generateKeys(DH *encryptionInfo) {
  int codes;
 
@@ -12,3 +17,35 @@ int generateKeys(DH *encryptionInfo) {
   
  return 0;
 }
+
+/**
+ * Calculate the math equation:
+ *  *result = (next/previous)^secret%p;
+ * 
+ * @param [out] result Common key from secret.
+ * @param [in] secret The secret key.
+ * @param [in] previous The previous participant public key.
+ * @param [in] next The next participant public key.
+ * @return 0 on sucess -1 on error
+ */
+int generateIntermediatekeys(BIGNUM *result, BIGNUM secret, BIGNUM previous, BIGNUM next, BIGNUM p){
+ BIGNUM rem;
+ BIGNUM dv;
+ BN_CTX *ctx = BN_CTX_new();
+ 
+ if(result == NULL) result=BN_new();
+ 
+ if(!BN_div(&rem, &dv, &next, &previous, ctx) ){
+    BN_CTX_free(ctx);
+    return -1;
+ }
+ 
+ if(!BN_mod_exp(result, &dv, &secret, &p, ctx)){
+     BN_CTX_free(ctx);
+     return -1;
+ }
+
+ BN_CTX_free(ctx);
+ return 0;
+}
+
--- dh.h
+++ dh.h
@@ -1,10 +1,24 @@
 #include <openssl/dh.h>
+#include <openssl/bn.h>
 
 #ifndef APP_DH
 
  /**
+  * Generate Diffie-Hellman Key Agreement keys.
   * @param [out] encryptionInfo The created Kerys Structure
+  * @return 0 on sucess -1 on failure
   */
  int generateKeys(DH *encryptionInfo);
 
+/**
+ * Calculate the math equation:
+ *  *result = (next/previous)^secret%p;
+ *
+ * @param [out] result Common key from secret.
+ * @param [in] secret The secret key.
+ * @param [in] previous The previous participant public key.
+ * @param [in] next The next participant public key.
+ * @return 0 on sucess -1 on error
+ */
+int generateIntermediatekeys(BIGNUM *result, BIGNUM secret, BIGNUM previous, BIGNUM next, BIGNUM p);
 #endif
