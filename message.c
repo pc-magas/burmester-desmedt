92e88f8f5e11dd94f9d62efe0dab0a96ea97f377
Dimitrios Desyllas (pc_magas) <you@example.com>
2019-01-25 18:49:44 +0200

    Common Message handling

---
+++ message.c
@@ -0,0 +1,83 @@
+#include "message.h"
+
+/**
+ * Broadcasts a bignum
+ * @param [in] num The number to broadcast
+ * @param [in] rank The id of the member
+ * @return -1 on error 0 on sucess
+ */
+int MPIbcastBigNum(BIGNUM num, int rank, char* puproce){
+    int size = BN_num_bytes(&num);
+    unsigned char *message = OPENSSL_malloc(size);
+
+    if(BN_bn2bin(&num, message) {
+     OPENSSL_free(message);
+     return -1;    
+    }
+
+    //Do the actual Broadcast && Debug
+    switch(MPI_Bcast((void *)message,size,MPI_BYTE,rank,MPI_COMM_WORLD)) {
+        case MPI_ERR_COMM:
+         fprintf(stderr, "RANK %d: COMMUNICATIPN ERROR on BIGNUM sending for purpoce \"%s\" \n", rank, purpoce);
+         fflush(stderr);
+         OPENSSL_free(message);
+         return -1;
+
+        case MPI_ERR_COUNT:
+         fprintf(stderr, "RANK %d: Invalid Size Count on BIGNUM sending for purpoce \"%s\" \n", rank, purpoce);
+         fflush(stderr);
+         OPENSSL_free(message);
+         return -1;
+        
+        case MPI_ERR_TYPE:
+         fprintf(stderr, "RANK %d: Invalid Data Type on BIGNUM sending for purpoce \"%s\" \n", rank, purpoce);
+         fflush(stderr);
+         OPENSSL_free(message);
+         return -1;
+
+        case MPI_ERR_BUFFER:
+         fprintf(stderr, "RANK %d: Invalid Buffer on BIGNUM sending for purpoce \"%s\" \n", rank, purpoce);
+         fflush(stderr);
+         OPENSSL_free(message);
+         return -1;
+
+        default:
+          printf("RANK %d: ALL OK on BIGNUM sending on purpoce \"%s\" \n", rank, purpoce);
+          fflush(stdin);
+    }
+    
+    //Cleanups
+    OPENSSL_free(message);
+    return 0;
+}
+
+/**
+ * Allocate a BigNumber array.
+ * @param [in] size The number of elements for the array
+ * @return An array to Store the BigNum
+ */
+// BIGNUM** allocateBigNumArray(int size){
+//     if(size<=0) return NULL;
+
+//     BIGNUM **array = OPENSSL_malloc(size*sizeof(BIGNUM*));
+
+//     if(!array) return NULL;
+
+//     for(int i=0;i<size;i++){
+//         array[i]=BN_new();
+//     }
+
+//     return array;
+// }
+
+/**
+ * Receive generated Big Number from all participants
+ * @param [out] numbers The list of all received Big Numbers
+ * @param [in] size The big number list
+ * 
+ */
+// int MPIReceiveBigNum(BIGNUM **numbers, int size) {
+//     numbers=allocateBigNumArray(size);
+//     if(numbers == NULL) return -1;
+
+// }
--- message.h
+++ message.h
@@ -1,8 +1,25 @@
+#include <openssl/bn.h>
+#include "mpi.h"
 
 /**
- * Message structure for publishing public keys
+ * Broadcasts a bignum
+ * @param [in] num The number to broadcast
+ * @param [in] rank The id of the member
+ * @return -1 on error 0 on sucess
  */
-typedef struct PubKeyMessage {
- char *publicDiffieHellmanKey;
- char *publicSignatureKey;
-} PubKeyMessage;
+int MPIbcastBigNum(BIGNUM num, int rank);
+
+/**
+ * Allocate a BigNumber array.
+ * @param [in] size The number of elements for the array
+ */
+// BIGNUM[] allocateBigNumArray(int size);
+
+
+/**
+ * Receive generated Big Number from all participants
+ * @param [out] numbers The list of all received Big Numbers
+ * @param [in] size The big number list
+ * @return -1 on error 0 on sucess
+ */
+// int MPIReceiveBigNum(BIGNUM **numbers, int size);
